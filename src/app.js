5e5b6bce705ae6f9d617d353de9bfc7e54e20e58
parahatreis <parahat.iljanow@gmail.com>
2020-07-17 18:55:34 +0500

    Remove expense data

--- src/app.js
+++ src/app.js
@@ -14,13 +14,6 @@ import moment from 'moment'
 const store = configureStore();
 
 
-store.dispatch(addExpense({description: 'Water Bill',amount:2000,createdAt:2010}));
-store.dispatch(addExpense({description: 'Gas Bill',amount:4500,createdAt:2000}));
-store.dispatch(addExpense({description: 'Air Bill',amount:8800,createdAt:2008}));
-
-const state = store.getState();
-const visibileExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibileExpenses)
 
 const jsx = (
    <Provider store={store}>
